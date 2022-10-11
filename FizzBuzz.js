918eb3d967584e917d78cc56d7dcc833b3f5b1cc
AldoBano89 <aldobano30@icloud.com>
2022-10-11 15:22:20 +0200

    first commit

---
+++ FizzBuzz.js
@@ -0,0 +1,11 @@
+var a = "Fizz"
+var b = "Buzz"
+var c = "FizzBuzz"
+    for ( var i=0; i<101; i++)
+    if ( i % 15 == 0) console.log (c)
+    else if (i % 5 == 0) console.log (b)
+    else if (i % 3 == 0) console.log (a)
+    else console.log(i)
+    
+    
+    
